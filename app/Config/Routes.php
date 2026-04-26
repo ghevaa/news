bb0fac4ea7ddd8e2379ec6c930aaa30a18c6c33c
Ghevary Pappo <ghevary@gmail.com>
2026-04-26 11:37:31 +0700

    refactor: fix the public routes to localhost:8080 instead

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Berita::index');
-$routes->get('/berita/(:num)', 'Berita::detail/$1');
+$routes->get('/berita', 'Berita::index');
+$routes->get('/berita/detail', 'Berita::detail');
 $routes->get('/profile', 'Berita::profile');
--- app/Views/berita/index.php
+++ app/Views/berita/index.php
@@ -17,30 +17,6 @@
                 </div>
             </div>
 
-            <div class="col-md-4 mb-4">
-                <div class="card h-100">
-                    <img src="https://images.unsplash.com/photo-1544367567-0f2fcb009e0b?auto=format&fit=crop&q=80&w=800" class="card-img-top" alt="Kesehatan Mental" style="height: 200px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title">Menjaga Kesehatan Mental di Era Digital</h5>
-                        <p class="card-text">Di era yang serba digital dan serba cepat ini, menjaga kesehatan mental adalah hal yang krusial...</p>
-                        <p class="card-text"><small class="text-muted">22 Apr 2026 14:30</small></p>
-                        <a href="<?= base_url('berita/detail') ?>" class="btn btn-primary mt-auto">Baca Selengkapnya</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4 mb-4">
-                <div class="card h-100">
-                    <img src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?auto=format&fit=crop&q=80&w=800" class="card-img-top" alt="Misi Mars" style="height: 200px; object-fit: cover;">
-                    <div class="card-body">
-                        <h5 class="card-title">Eksplorasi Luar Angkasa: Misi Mars Terbaru</h5>
-                        <p class="card-text">Badan antariksa dunia baru saja meluncurkan misi terbarunya ke planet merah...</p>
-                        <p class="card-text"><small class="text-muted">20 Apr 2026 10:00</small></p>
-                        <a href="<?= base_url('berita/detail') ?>" class="btn btn-primary mt-auto">Baca Selengkapnya</a>
-                    </div>
-                </div>
-            </div>
-
         </div>
     </div>
 </div>
